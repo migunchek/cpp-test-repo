8bc8f69e0b7a3f2150210439cff86f398b2b490a
migunchek <migunchek@gmail.com>
2025-01-28 08:49:46 +0300

    Доработка

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+class ClassA
+{
+private:
+    /* data */
+public:
+    ClassA(/* args */);
+    ~ClassA();
+};
+
+ClassA::ClassA(/* args */)
+{
+}
+
+ClassA::~ClassA()
+{
+}
+
+
 int main() {
     std::cout << "Hello, Git!" << std::endl;
     int a = 0;
+    int b = 0;
 }
